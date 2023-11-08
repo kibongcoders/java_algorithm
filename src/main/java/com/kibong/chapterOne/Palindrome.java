6d65d574847ec31a3fac1cbd8ede9fb69eb0fd4d
kibong95 <kibongcoders>
2023-11-08 13:57:33 +0900

    Init

--- src/main/java/com/kibong/chapterOne/Palindrome.java
+++ src/main/java/com/kibong/chapterOne/Palindrome.java
@@ -1,2 +1,31 @@
-package com.kibong.chapterOne;public class Palindrome {
+package com.kibong.chapterOne;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String test = "Do geese see God?";
+
+        int start = 0;
+        int end = test.length()-1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if(!Character.isLetterOrDigit(test.charAt(start))){
+                start++;
+            }else if(!Character.isLetterOrDigit(test.charAt(end))){
+                end--;
+            }else{
+                if(Character.toLowerCase(test.charAt(start)) != Character.toLowerCase(test.charAt(end))){
+                    isPalindrome = false;
+                    break;
+                }else{
+                    start++;
+                    end--;
+                }
+            }
+        }
+
+        System.out.println("answer = " + isPalindrome);
+    }
 }
